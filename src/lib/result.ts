5963a372c65dc5e9869e540c8ecbe1b18773ac4b
sakurawen <wisakura@outlook.com>
2025-10-14 16:50:19 +0800

    feat: result add code

--- src/lib/result.ts
+++ src/lib/result.ts
@@ -2,6 +2,7 @@ import { merge } from 'lodash-es';
 
 export function result<T extends object, E extends object>(data: T, extra: E = {} as E) {
   return merge({
+    code: '',
     success: true,
     data,
     message: '',
