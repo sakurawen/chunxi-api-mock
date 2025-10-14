bca3c6259409d9d8c6878fea4f87aadee339aa03
sakurawen <wisakura@outlook.com>
2025-10-14 21:13:45 +0800

    feat: add cors plugin

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 import { commonRouter } from './routes/common/route.js';
 import { emrRouter } from './routes/emr/route.js';
 import { hospitalRouter } from './routes/hospital/route.js';
 import { patientRouter } from './routes/patient/route.js';
 import { userRouter } from './routes/user/route.js';
 
-const app = new Hono()
-  .route('/common', commonRouter)
-  .route('/patient', patientRouter)
-  .route('/hospital', hospitalRouter)
-  .route('/user', userRouter)
-  .route('/emr', emrRouter);
+const app = new Hono().use(cors({
+  origin: '*',
+  allowHeaders: ['Content-Type', 'Authorization'],
+  allowMethods: ['POST', 'GET', 'OPTIONS'],
+  exposeHeaders: ['Content-Length'],
+  maxAge: 600,
+  credentials: true,
+})).route('/common', commonRouter).route('/patient', patientRouter).route('/hospital', hospitalRouter).route('/user', userRouter).route('/emr', emrRouter);
 
 serve({
   fetch: app.fetch,
